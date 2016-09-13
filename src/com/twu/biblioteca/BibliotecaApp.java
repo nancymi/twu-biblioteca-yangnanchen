e03f7fbcc00539828278c869fbd4c070ade0c745
nancymi <13121178nancy@gmail.com>
2016-09-13 21:29:31 +0800

    Finish: print welcome message feature

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,4 +5,9 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public String welcome() {
+        String welcomeMsg = "welcome!";
+        return welcomeMsg;
+    }
 }
