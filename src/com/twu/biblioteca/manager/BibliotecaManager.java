839e56b7a5b1a6a7b79e4ccb0f3db13a5e0fdb1d
nancymi <13121178nancy@gmail.com>
2016-09-19 23:57:43 +0800

    Update: give more function to operate books

---
+++ src/com/twu/biblioteca/manager/BibliotecaManager.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca.manager;
+
+/**
+ * Created by ncyang on 9/19/16.
+ */
+public class BibliotecaManager {
+}
---
+++ test/com/twu/biblioteca/manager/CustomerManagerTest.java
@@ -0,0 +1,10 @@
+package com.twu.biblioteca.manager;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ncyang on 9/19/16.
+ */
+public class CustomerManagerTest {
+
+}
